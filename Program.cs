fce47ad252a6bf39588852ecaeb6287eb8a375e8
aemelyanov92 <109788017+aemelyanov92@users.noreply.github.com>
2022-08-12 13:05:53 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,72 +1,90 @@
-﻿Семинар 1. С#
-Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-Console.Write("Введите первое число ");
-int num1 = Convert.ToInt32(Console.ReadLine());
+﻿// // Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// // Нужно сделать через числа, без индексов строк.
+// // 456 -> 5
+// // 782 -> 8
+// // 918 -> 1
+// int number = new Random().Next (100,1000);
+// Console.WriteLine(number);
+// int secondDigit = ((number % 100) / 10);
+// Console.WriteLine($"{secondDigit}");
 
-Console.Write("Введите второе число ");
-int num2 = Convert.ToInt32(Console.ReadLine());
-int max;
-int min;
-if (num1 > num2)
-{
-max = num1;
-min = num2;               
-}
-else
-{
-max = num2;
-min = num1;
-}
-Console.WriteLine("макс число : {0}", max);
-Console.WriteLine("мин. число : {0}", min);
+// // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// // 645 -> 5
+// // 78 -> третьей цифры нет
+// // 32679 -> 6
 
-Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-Console.WriteLine("Нахождение наибольшего из трех чисел");
-Console.Write("Введите первое число ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе число ");
-int num2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите третье число ");
-int num3 = Convert.ToInt32(Console.ReadLine());
-int max = num1;
-if (num1>num2)
-{
-    max = num1;
-    
-}
-else
-{
-    max = num2;
-}
-if (num2 > num3)
-{
-    max = num2;
-}
-else
-{
-    max = num3;  
-}
-Console.WriteLine("Максимальное из трёх чисел = " + max);
+// Console.WriteLine("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// string txtnum = Convert.ToString(number);
+// if (txtnum.Length>2) 
+// {
+//     Console.WriteLine(txtnum[2]);
+// }
+// else 
+// {
+//     Console.WriteLine("Нет третьей цифры, введите другое число");
+// }    
 
 
-Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// // Напишите программу, которая будет выдавать название дня недели
+// //  по заданному номеру!
+// // 3 -> Среда 
+// // 5 -> Пятница
 
-4 -> да
--3 -> нет
-7 -> нет
-Console.Write("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
+// int dayNumber = Convert.ToInt32(Console.ReadLine());
 
-if (number % 2 == 0) Console.WriteLine("Число делится на 2");
-else Console.WriteLine("Остаток равен " + number%2);
+// if (dayNumber == 1)
+// {
+//     System.Console.WriteLine("Понедельник");
+// }
+// if (dayNumber == 2)
+// {
+//     System.Console.WriteLine("Вторник");
+// }
+// else
+// {
+//    System.Console.WriteLine("error 404");     
+// }
 
 
-Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
-5 -> 2, 4
-8 -> 2, 4, 6, 8
-Console.Write("Введите число: ");
-int N = Convert.ToInt32(Console.ReadLine()); 
-for (int x = 1; x < N+1; x++)       
-if (x%2 == 0)            
-Console.WriteLine(x);
+// // Задача 15: Напишите программу, которая принимает на вход цифру, 
+// // обозначающую день недели, и проверяет, является ли этот день выходным.
+// // 6 -> да
+// // 7 -> да
+// // 1 -> нет
+// // Console.WriteLine("Введите число: ");
+
+// // int day = Convert.ToInt32(Console.ReadLine());
+
+// // if (day<6)
+// // {
+// //     Console.WriteLine("Это будний день");
+// // }
+
+// // else
+// // {
+// //     Console.WriteLine("Это выходной");
+// // }
+
+
+// // Но я думаю что надо лучше так:
+
+// // Version 2.1 Modified
+
+// // Console.WriteLine("Введите число: ");
+
+// // int day = Convert.ToInt32(Console.ReadLine());
+
+// // if (day<6)
+// // {
+// //     Console.WriteLine("Это будний день");
+// // }
+// // else if(day>7)
+// // {
+// //     Console.WriteLine("Error 404, так не бывает");
+// // }
+// // else
+// // {
+// //     Console.WriteLine("Это выходной");
+// // }
